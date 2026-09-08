6e9f3f740ad9ada42721943ac7646a1884b510db
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-09-08 17:32:35 +0300

    add task 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,6 @@
-
+#!/usr/bin/env python3
+def index_range(page, page_size):
+    start_index = (page-1)*page_size
+    end_index= page*page_size
+    tuble = (start_index, end_index)
+    return tuble

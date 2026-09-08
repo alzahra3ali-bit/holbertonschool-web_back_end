8abf23a593600abf8b62fc48308f27b123a7a046
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-09-08 17:52:01 +0300

    add task 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""Simple helper function for pagination"""
 from typing import Tuple
-def index_range(page : int, page_size : int) -> Tuple[int,int]:
-    start_index = (page-1) * page_size
-    end_index = page * page_size
+"""
+    Calculate start and end indexes for a given page and page_size.
+"""
+def index_range(page: int, page_size: int) -> Tuple[int,int]:
+    start_index= (page-1)*page_size
+    end_index= page*page_size
     tuple = (start_index, end_index)
     return tuple

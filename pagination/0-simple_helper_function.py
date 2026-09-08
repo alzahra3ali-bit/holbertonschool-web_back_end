5b2995ccddea71c426fb409a7441ee0496ae2ae7
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-09-08 17:58:17 +0300

    add task 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,11 +3,14 @@
 Simple helper function module for pagination.
 """
 from typing import Tuple
+
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-"""
+    """
     Calculate start and end indexes for a given page and page_size.
-"""
-    start_index= (page-1) * page_size
-    end_index= page *page_size
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
     tupl = (start_index, end_index)
     return tupl
+    

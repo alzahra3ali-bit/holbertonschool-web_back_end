ccb54239c97302ed61778fab10a4d8e08e430c70
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-09-08 17:37:22 +0300

    add task 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-def index_range(page, page_size):
-    start_index = (page-1)*page_size
-    end_index= page*page_size
-    tuble = (start_index, end_index)
-    return tuble
+def index_range(page : int, page_size : int) -> Tuple[int:int]:
+    start_index = (page-1) * page_size
+    end_index = page * page_size
+    tuple = (start_index, end_index)
+    return tuple

c1676dc148d78b31fc9436ee48247668d9c97f77
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-09-11 21:18:35 +0300

    Task 0

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to update topics of a school document
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    
